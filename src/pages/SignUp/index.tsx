75f728d8c3a0848abe0c43f0d582eba29ad37e15
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2020-09-11 00:04:41 -0300

    add Components Button, Input and page SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
@@ -8,14 +8,17 @@ import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
+const SignUp: React.FC = () => (
   <Container>
+    <Background />
+
     <Content>
       <img src={logo} alt="GoBarber" />
 
       <form>
         <h1>Faça seu logon</h1>
 
+        <Input name="name" icon={FiUser} placeholder="Nome" />
         <Input name="email" icon={FiMail} placeholder="E-mail" />
         <Input
           name="password"
@@ -24,18 +27,15 @@ const SignIn: React.FC = () => (
           type="password"
         />
 
-        <Button type="submit">Entrar</Button>
-
-        <a href="/forgot">Esqueci minha senha</a>
+        <Button type="submit">Cadastrar</Button>
       </form>
 
       <a href="/login">
-        <FiLogIn />
-        Criar conta
+        <FiArrowLeft />
+        Voltar para logon
       </a>
     </Content>
-    <Background />
   </Container>
 );
 
-export default SignIn;
+export default SignUp;
